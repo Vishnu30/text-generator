643a848713dd2111c361bc2e371ac470f03ae08b
vishnubajpai_cybersafe <vishnu.bajpai@cybersafe.co.in>
2017-10-20 15:17:45 +0530

    commit code

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+import Output from './Components/Output';
+import Select from './Components/Controls/Select';
+import Text from './Components/Controls/Text';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      paras: 4,
+      format: 'html',
+      text: ''
+    }
+  }
+
+  componentWillMount(){
+    this.getSampleText();
+  }
+
+  getSampleText(){
+    axios.get('https://baconipsum.com/api/?type=meat-and-filter&format='+this.state.format+'&paras='+this.state.paras)
+      .then((response) => {
+        this.setState({text: response.data}, function(){
+          console.log(this.state);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  
+   showHtml(x){
+      this.setState({format: x}, this.getSampleText);
+  }
+
+  changeParas(number){
+      this.setState({paras: number}, this.getSampleText);
+  }
+
+  render() {
+    return (
+      <div className="App container">
+       <h1 className="text-center">ReactJS Sample Text Generator</h1>
+        <hr />
+        <form className="form-inline">
+          <div className="form-group">
+            <label>Paragraphs:</label>
+            <Text value={this.state.paras} onChange={this.changeParas.bind(this)} />
+          </div>
+          <div className="form-group">
+            <label>Select Format:</label>
+            <Select value={this.state.format} onChange={this.showHtml.bind(this)} />
+          </div>
+        </form>
+        <br /><br />
+        <Output value={this.state.text} />
+      </div>
+    );
+  }
+}
+
+export default App;
